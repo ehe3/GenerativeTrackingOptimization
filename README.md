32ee0ea85819ce5c8c777c0079efd8ebdf9fb893
Eric He <eric.he3@gmail.com>
2019-02-11 14:51:00 -0500

    first commit: created an OpenGL context where render to texture is implemented with a depth map -- r2t with color buffer also in commented code

---
+++ README.md
@@ -0,0 +1,1 @@
+Takes in per frame discrimitive model and depth image information and outputs a prediction through a golden energy calculation and PSO(/LM) optimization.
---
+++ res/shaders/FragmentShader.glsl
@@ -0,0 +1,12 @@
+#version 310 es
+
+precision mediump float;
+
+layout(location = 0) out vec4 color;
+
+//uniform vec4 u_Color;
+
+void main()
+{
+	color = vec4(1.0, 1.0, 1.0, 1.0);
+}
