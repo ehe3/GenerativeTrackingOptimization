d3bb50a0749837ecf64e62d522fa32486a2a5361
Eric He <eric.he3@gmail.com>
2019-02-25 19:12:00 -0500

    PSO with instanced rendering

--- src/pso.h
+++ src/pso.h
@@ -97,9 +97,9 @@ class PSO {
 		Shader RepeatShader, SubtractionShader, RTTShader, R2Shader, R4X3YShader, R5Shader, PTShader;
 		Model footModel;
 		// quads, textures, and buffers
-		GLuint quadVAO, quadVBO, repeatQuadVAO, repeatQuadVBO, refdepthtex, ping, depthtexture, pong, repeattex, peng, difftex, pang, tex320x240, pung, tex160x120, pling, tex80x60, plang, tex40x30, plong, tex20x15, plung, tex5x5, pleng, tex1x1;
-		// instance buffer
-		GLuint instanceVBO;
+		GLuint quadVAO, quadVBO, repeatQuadVAO, repeatQuadVBO, refdepthtex, peng, repeattex, ping, depthtexture, pong, difftex, pang, tex320x240, pung, tex160x120, pling, tex80x60, plang, tex40x30, plong, tex20x15, plung, tex5x5, pleng, tex1x1;
+		// instance buffers
+		GLuint instanceVBO, transformationInstanceBuffer;
 
 	public:
 		PSO(int numParticles, float CogConst=2.8, float SocConst=1.3) : 
@@ -133,7 +133,7 @@ class PSO {
 
 			// Make the window's context current and then hide it
 			glfwMakeContextCurrent(window);
-			//glfwHideWindow(window);
+			glfwHideWindow(window);
 
 			// Initialize GLEW
 			glewExperimental = true;
@@ -155,24 +155,47 @@ class PSO {
 
 			// Load the foot model 
 			footModel = Model("../res/foot.obj");
-			Mesh footMesh = footModel.meshes[0];
 
-			glm::vec2 translations[numParticles];
+			float translations[numParticles];
 			for (int i = 0; i < NumParticles; i++)
 			{
-				translations[i] = glm::vec2(i*2.0/NumParticles, 0);
+				translations[i] = i*2.0/NumParticles;
 			}
 
-			// set up the instance VBO
+			// set up the instance VBO for offsets
 			glGenBuffers(1, &instanceVBO);
 			glBindBuffer(GL_ARRAY_BUFFER, instanceVBO);
-			glBufferData(GL_ARRAY_BUFFER, sizeof(glm::vec2)*NumParticles, &translations[0], GL_STATIC_DRAW);
+			glBufferData(GL_ARRAY_BUFFER, sizeof(float)*NumParticles, &translations[0], GL_STATIC_DRAW);
 
-			glBindVertexArray(footMesh.VAO);
+			glBindVertexArray(footModel.meshes[0].VAO);
 			glEnableVertexAttribArray(5);
-			glVertexAttribPointer(5, 2, GL_FLOAT, GL_FALSE, 2*sizeof(float), (void*)0);
+			glVertexAttribPointer(5, 1, GL_FLOAT, GL_FALSE, sizeof(float), (void*)0);
 			glBindBuffer(GL_ARRAY_BUFFER, 0);
 			glVertexAttribDivisor(5, 1);
+
+			// set up the instance VBO for model matricies
+			glGenBuffers(1, &transformationInstanceBuffer);
+			glBindBuffer(GL_ARRAY_BUFFER, transformationInstanceBuffer);
+			glBufferData(GL_ARRAY_BUFFER, NumParticles*sizeof(glm::mat4), nullptr, GL_STATIC_DRAW);
+			
+			glBindVertexArray(footModel.meshes[0].VAO);
+			GLsizei vec4Size = sizeof(glm::vec4);
+
+			glEnableVertexAttribArray(6);
+			glVertexAttribPointer(6, 4, GL_FLOAT, GL_FALSE, 4*vec4Size, (void*)0);
+			glEnableVertexAttribArray(7);
+			glVertexAttribPointer(7, 4, GL_FLOAT, GL_FALSE, 4*vec4Size, (void*)(vec4Size));
+			glEnableVertexAttribArray(8);
+			glVertexAttribPointer(8, 4, GL_FLOAT, GL_FALSE, 4*vec4Size, (void*)(2*vec4Size));
+			glEnableVertexAttribArray(9);
+			glVertexAttribPointer(9, 4, GL_FLOAT, GL_FALSE, 4*vec4Size, (void*)(3*vec4Size));
+			glBindBuffer(GL_ARRAY_BUFFER, 0);
+			glVertexAttribDivisor(6, 1);
+			glVertexAttribDivisor(7, 1);
+			glVertexAttribDivisor(8, 1);
+			glVertexAttribDivisor(9, 1);
+
+			glBindVertexArray(0);
 
 			float quadVertices[] = {
 				// positions   // texCoords
@@ -196,7 +219,7 @@ class PSO {
 				1.0f,  1.0f,  1.0f*NumParticles, 0.0f
 			};
 
-			// Declare quad
+			// declare quad
 			glGenVertexArrays(1, &quadVAO);
 			glGenBuffers(1, &quadVBO);
 			glBindVertexArray(quadVAO);
@@ -209,7 +232,7 @@ class PSO {
 
 			glBindVertexArray(0);
 
-			// Declare repeat quad
+			// declare repeat quad
 			glGenVertexArrays(1, &repeatQuadVAO);
 			glGenBuffers(1, &repeatQuadVBO);
 			glBindVertexArray(repeatQuadVAO);
@@ -226,20 +249,20 @@ class PSO {
 			glGenFramebuffers(1, &peng);
 			glBindFramebuffer(GL_FRAMEBUFFER, peng);
 
-			// texture that is the difference of reference and rendered
-			glGenTextures(1, &difftex);
+			// reference depth map on repeat
+			glGenTextures(1, &repeattex);
 			glActiveTexture(GL_TEXTURE0 + 1);
-			glBindTexture(GL_TEXTURE_2D, difftex);
+			glBindTexture(GL_TEXTURE_2D, repeattex);
 			glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, NumParticles*640, 480, 0, GL_DEPTH_COMPONENT, GL_FLOAT, 0);
 			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);	
 			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);	
-			glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, difftex, 0);
+			glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, repeattex, 0);
 			
 			// set up ping
 			glGenFramebuffers(1, &ping);
 			glBindFramebuffer(GL_FRAMEBUFFER, ping);
 
-			// loaded reference depth texture
+			// rendered models in line
 			glGenTextures(1, &depthtexture);
 			glActiveTexture(GL_TEXTURE0 + 2);
 			glBindTexture(GL_TEXTURE_2D, depthtexture);
@@ -253,13 +276,13 @@ class PSO {
 			glBindFramebuffer(GL_FRAMEBUFFER, pong);
 
 			// texture that is the difference of reference and rendered
-			glGenTextures(1, &repeattex);
+			glGenTextures(1, &difftex);
 			glActiveTexture(GL_TEXTURE0 + 3);
-			glBindTexture(GL_TEXTURE_2D, repeattex);
+			glBindTexture(GL_TEXTURE_2D, difftex);
 			glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, NumParticles*640, 480, 0, GL_DEPTH_COMPONENT, GL_FLOAT, 0);
 			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);	
 			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);	
-			glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, repeattex, 0);
+			glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, difftex, 0);
 		
 			// set up pang 
 			glGenFramebuffers(1, &pang);
@@ -364,46 +387,36 @@ class PSO {
 			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);	
 			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 
-			glm::mat4* Movements = new glm::mat4[NumParticles];
-			for (int i = 0; i < NumParticles; i++)
-			{
-				PoseParameters currparam = parameterList[i];
-				// Set up MVP matricies
-				glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(currparam.XTranslation, currparam.YTranslation, currparam.ZTranslation));
-				model = glm::rotate(glm::rotate(glm::rotate(model, currparam.XRotation, glm::vec3(1, 0, 0)), currparam.YRotation, glm::vec3(0, 1, 0)), currparam.ZRotation, glm::vec3(0, 0, 1));
-				Movements[i] = model;	
-			}
-
-			// Set up model transformations as instances
-			GLuint transformationInstanceBuffer;
-			glGenBuffers(1, &transformationInstanceBuffer);
-			glBindBuffer(GL_ARRAY_BUFFER, transformationInstanceBuffer);
-			glBufferData(GL_ARRAY_BUFFER, NumParticles*sizeof(glm::mat4), &Movements[0], GL_STATIC_DRAW);
-
-			Mesh footMesh = footModel.meshes[0];
-			glBindVertexArray(footMesh.VAO);
-			GLsizei vec4Size = sizeof(glm::vec4);
-
-			glEnableVertexAttribArray(6);
-			glVertexAttribPointer(6, 4, GL_FLOAT, GL_FALSE, 4 * vec4Size, (void*)0);
-			glEnableVertexAttribArray(7);
-			glVertexAttribPointer(7, 4, GL_FLOAT, GL_FALSE, 4 * vec4Size, (void*)(vec4Size));
-			glEnableVertexAttribArray(8);
-			glVertexAttribPointer(8, 4, GL_FLOAT, GL_FALSE, 4 * vec4Size, (void*)(2*vec4Size));
-			glEnableVertexAttribArray(9);
-			glVertexAttribPointer(9, 4, GL_FLOAT, GL_FALSE, 4 * vec4Size, (void*)(3*vec4Size));
-
-			glBindBuffer(GL_ARRAY_BUFFER, 0);
-			glVertexAttribDivisor(6, 1);
-			glVertexAttribDivisor(7, 1);
-			glVertexAttribDivisor(8, 1);
-			glVertexAttribDivisor(9, 1);
-
-			glBindVertexArray(0);
 
 			glEnable(GL_DEPTH_TEST);
-			while (!glfwWindowShouldClose(window))
+
+			// Intialize particles
+			Particle* particles = new Particle[NumParticles];
+			for (int i = 0; i < NumParticles; i++)
 			{
+				particles[i].Position = parameterList[i];
+			}
+			
+			// Time the PSO without setup
+			auto start = std::chrono::high_resolution_clock::now();
+			// Setup finished, start the particle swarm!
+			PoseParameters GlobalBestPosition;
+			float GlobalBestEnergy = std::numeric_limits<float>::infinity();
+
+			for (int generation = 0; generation < iters; generation++)
+			{
+				glm::mat4* Movements = new glm::mat4[NumParticles];
+				for (int i = 0; i < NumParticles; i++)
+				{
+					PoseParameters currparam = particles[i].Position;
+					// Set up MVP matricies
+					glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(currparam.XTranslation, currparam.YTranslation, currparam.ZTranslation));
+					model = glm::rotate(glm::rotate(glm::rotate(model, currparam.XRotation, glm::vec3(1, 0, 0)), currparam.YRotation, glm::vec3(0, 1, 0)), currparam.ZRotation, glm::vec3(0, 0, 1));
+					Movements[i] = model;	
+				}
+
+				glNamedBufferSubData(transformationInstanceBuffer, 0, NumParticles*sizeof(glm::mat4), &Movements[0]);
+			
 				RepeatShader.use();
 				RepeatShader.setInt("tex", 0);
 				glBindFramebuffer(GL_FRAMEBUFFER, peng);
@@ -413,7 +426,7 @@ class PSO {
 
 				glEnable(GL_DEPTH_TEST);
 				//Send matricies to shader
-				RTTShader.use(); // Check to see if this needs to be called every time
+				RTTShader.use(); 
 				RTTShader.setMat4("u_P", ProjMat);
 				RTTShader.setInt("instances", NumParticles);
 				RTTShader.setFloat("zNear", 0.05f);
@@ -506,156 +519,36 @@ class PSO {
 				glDrawArrays(GL_TRIANGLES, 0, 6);
 
 				float* currentdt = new float[NumParticles];
-				glGetTextureImage(tex1x1, 0, GL_DEPTH_COMPONENT, GL_FLOAT, sizeof(float)*6, currentdt);
+				glGetTextureImage(tex1x1, 0, GL_DEPTH_COMPONENT, GL_FLOAT, sizeof(float)*NumParticles, currentdt);
 
-				for (int i = 0; i < NumParticles; i++)
-				{
-					std::cout << currentdt[i] << std::endl;
-				}
-
-				break;
-				glfwSwapBuffers(window);
-				glfwPollEvents();
-				GLCheckError();
-			}
-
-			return PoseParameters();
-
-			// Intialize particles
-			Particle* particles = new Particle[NumParticles];
-			for (int i = 0; i < NumParticles; i++)
-			{
-				particles[i].Position = parameterList[i];
-			}
-			
-			// Time the PSO without setup
-			auto start = std::chrono::high_resolution_clock::now();
-			// Setup finished, start the particle swarm!
-			PoseParameters GlobalBestPosition;
-			float GlobalBestEnergy = std::numeric_limits<float>::infinity();
-
-			for (int generation = 0; generation < iters; generation++)
-			{
+				// first loop to update local bests and global best
 				for (int p = 0; p < NumParticles; p++)
 				{
-					// Set up MVP matricies
-					glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(particles[p].Position.XTranslation, particles[p].Position.YTranslation, particles[p].Position.ZTranslation));
-					model = glm::rotate(glm::rotate(glm::rotate(model, particles[p].Position.XRotation, glm::vec3(1, 0, 0)), particles[p].Position.YRotation, glm::vec3(0, 1, 0)), particles[p].Position.ZRotation, glm::vec3(0, 0, 1));
-
-					//Send matricies to shader
-					RTTShader.use(); // Check to see if this needs to be called every time
-					RTTShader.setMat4("u_P", ProjMat);
-					RTTShader.setFloat("zNear", 0.05f);
-					RTTShader.setFloat("zFar", 1.0f);
-
-					glEnable(GL_DEPTH_TEST);
-					glBindFramebuffer(GL_FRAMEBUFFER, ping);
-					glClear(GL_DEPTH_BUFFER_BIT);
-
-					RTTShader.use();
-					footModel.Draw(RTTShader);
-
-					glBindFramebuffer(GL_FRAMEBUFFER, pong);
-					glClear(GL_DEPTH_BUFFER_BIT);
-					SubtractionShader.use();
-					SubtractionShader.setInt("screenTexture", 0);
-					SubtractionShader.setInt("gendepTexture", 1);
-					glBindVertexArray(quadVAO);
-					glDrawArrays(GL_TRIANGLES, 0, 6);
-
-					glBindFramebuffer(GL_FRAMEBUFFER, pang);
-					glClear(GL_DEPTH_BUFFER_BIT);
-					R2Shader.use();
-					R2Shader.setInt("tex", 2);
-					R2Shader.setFloat("width", 640.0f);
-					R2Shader.setFloat("height", 480.0f);
-					glDrawArrays(GL_TRIANGLES, 0 , 6);
-
-					glBindFramebuffer(GL_FRAMEBUFFER, pung);
-					glClear(GL_DEPTH_BUFFER_BIT);
-					R2Shader.use();
-					R2Shader.setInt("tex", 3);
-					R2Shader.setFloat("width", 320.0f);
-					R2Shader.setFloat("height", 240.0f);
-					glViewport(0, 0, 320, 240);
-					glDrawArrays(GL_TRIANGLES, 0 , 6);
-
-					glBindFramebuffer(GL_FRAMEBUFFER, pling);
-					glClear(GL_DEPTH_BUFFER_BIT);
-					R2Shader.use();
-					R2Shader.setInt("tex", 4);
-					R2Shader.setFloat("width", 160.0f);
-					R2Shader.setFloat("height", 120.0f);
-					glViewport(0, 0, 160, 120);
-					glDrawArrays(GL_TRIANGLES, 0 , 6);
-
-					glBindFramebuffer(GL_FRAMEBUFFER, plang);
-					glClear(GL_DEPTH_BUFFER_BIT);
-					R2Shader.use();
-					R2Shader.setInt("tex", 5);
-					R2Shader.setFloat("width", 80.0f);
-					R2Shader.setFloat("height", 60.0f);
-					glViewport(0, 0, 80, 60);
-					glDrawArrays(GL_TRIANGLES, 0 , 6);
-
-					glBindFramebuffer(GL_FRAMEBUFFER, plong);
-					glClear(GL_DEPTH_BUFFER_BIT);
-					R2Shader.use();
-					R2Shader.setInt("tex", 6);
-					R2Shader.setFloat("width", 40.0f);
-					R2Shader.setFloat("height", 30.0f);
-					glViewport(0, 0, 40, 30);
-					glDrawArrays(GL_TRIANGLES, 0 , 6);
-
-					glBindFramebuffer(GL_FRAMEBUFFER, plung);
-					glClear(GL_DEPTH_BUFFER_BIT);
-					R4X3YShader.use();
-					R4X3YShader.setInt("tex", 7);
-					R4X3YShader.setFloat("width", 20.0f);
-					R4X3YShader.setFloat("height", 15.0f);
-					glViewport(0, 0, 20, 15);
-					glDrawArrays(GL_TRIANGLES, 0 , 6);
-						
-					glBindFramebuffer(GL_FRAMEBUFFER, pleng);
-					glClear(GL_DEPTH_BUFFER_BIT);
-					R5Shader.use();
-					R5Shader.setInt("tex", 8);
-					R5Shader.setFloat("width", 5.0f);
-					R5Shader.setFloat("height", 5.0f);
-					glViewport(0, 0, 5, 5);
-					glDrawArrays(GL_TRIANGLES, 0 , 6);
-					
-					glBindFramebuffer(GL_FRAMEBUFFER, 0);
-					glClear(GL_DEPTH_BUFFER_BIT);
-					PTShader.use();
-					PTShader.setInt("tex", 9);
-					glViewport(0, 0, 640, 480);
-					glBindVertexArray(quadVAO);
-					glDrawArrays(GL_TRIANGLES, 0, 6);
-
-					float* currentdt = new float[1];
-					glGetTextureImage(tex1x1, 0, GL_DEPTH_COMPONENT, GL_FLOAT, sizeof(float), currentdt);
-					float currentIndividualEnergy = currentdt[0];	
-
-					std::cout << "cie: " << currentIndividualEnergy << std::endl;
-					std::cout << "cbes for particle " << p << ": " << particles[p].BestEnergyScore << std::endl;
-					if (currentIndividualEnergy < particles[p].BestEnergyScore)
+					if (currentdt[p] < particles[p].BestEnergyScore)
 					{
-						particles[p].BestEnergyScore = currentIndividualEnergy;
+						particles[p].BestEnergyScore = currentdt[p];
 						particles[p].BestPosition = particles[p].Position;
-						if (currentIndividualEnergy < GlobalBestEnergy)
+						if (currentdt[p] < GlobalBestEnergy)
 						{
-							GlobalBestEnergy = currentIndividualEnergy;
+							GlobalBestEnergy = currentdt[p];
 							GlobalBestPosition = particles[p].Position;
 						}
+						std::cout << "cie: " << currentdt[p] << std::endl;
+						std::cout << "cbes for particle " << p << ": " << particles[p].BestEnergyScore << std::endl;
 					}
-					std::cout << "gbe: " << GlobalBestEnergy << std::endl;
+				}
+				std::cout << "gbe: " << GlobalBestEnergy << std::endl;
+
+				// second loop to update position and velocities
+				for (int p = 0; p < NumParticles; p++)
+				{
 					float r1 = ((float) std::rand() / RAND_MAX);
 					float r2 = ((float) std::rand() / RAND_MAX);
 					particles[p].Velocity = particles[p].Velocity + ((particles[p].BestPosition - particles[p].Position)*CognitiveConst*r1 + (GlobalBestPosition - particles[p].Position)*SocialConst*r2)*ConstrictionConst;
 					particles[p].Position = particles[p].Position + particles[p].Velocity; 
 				}
 			}
+
 			auto end = std::chrono::high_resolution_clock::now();
 			std::cout << "Time it took for PSO to execute without OpenGL setup is: " << std::chrono::duration_cast<std::chrono::milliseconds> (end-start).count() << std::endl;
 			return GlobalBestPosition;
